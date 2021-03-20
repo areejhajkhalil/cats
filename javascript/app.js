c4c0754958dc918cc5e7bb27c88e7c3b2e21c7ba
areejhajkhalil <areej.hajkhalil@gmail.com>
2021-03-21 01:26:16 +0200

    add new changes

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 prompt("what is your favorite type of cat?")
 
-if(favouritcat=='Russian'){document.write('<img src="pictures/Russian.jfif"/>');}
+if(favouritcat=='russian'){document.write('<img src="pictures/russian.jfif"/>');}
 else if(favoritecat=='shirazi'){document.write('<img src="pictures/shirazi.jfif"/>');}
 else{alert('choose between Russian or shirazi')}
 
