df4e86c590d8d86a71dc9907b153313d7c044011
areejhajkhalil <areej.hajkhalil@gmail.com>
2021-03-21 15:49:32 +0200

    add modified to some files

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,25 @@
-prompt("what is your favorite type of cat?")
+var favouritcat= prompt("what is your favorite type of cat?");
 
-if(favouritcat=='russian'){document.write('<img src="pictures/russian.jfif"/>');}
-else if(favoritecat=='shirazi'){document.write('<img src="pictures/shirazi.jfif"/>');}
-else{alert('choose between Russian or shirazi')}
+while (favouritcat!=='russian'&&favouritcat!=='shirazi'){favouritcat= prompt('please only write russian/shirazi');}
+
+var numberOfpictures=prompt('how many pictures do you want?');
+
+var pictures ='';
+var result = '';
 
 
 
+if(favouritcat=='russian'){pictures = '<img src="pictures/russian.jfif"/>';}
+else if(favouritcat=='shirazi'){pictures = '<img src="pictures/shirazi.jfif"/>';}
+else{alert('choose between russian or shirazi');}
+
+for(var i=0;i<numberOfpictures;i++){result+=pictures;}
+
+var amount =prompt("what is the amount that you can pay?");
+if (amount >= 100) {alert('you can pay')}
+else {alert('you caant buy')}
+ 
+document.write(result);
 
 
 
