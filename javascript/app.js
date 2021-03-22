34413a6f91229c52486152c105060a3a22ba5c08
areejhajkhalil <areej.hajkhalil@gmail.com>
2021-03-22 16:36:08 +0200

    add new modifications

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,9 @@
-var favouritcat= prompt("what is your favorite type of cat?");
+var favouritcat = prompt("what is your favorite type of cat?");
 
-while (favouritcat!=='russian'&&favouritcat!=='shirazi'){favouritcat= prompt('please only write russian/shirazi');}
+var getfavouritcat = function (userAnswer) {
+
+
+while ( favouritcat !== 'russian' && favouritcat !== 'shirazi' ) { favouritcat = prompt('please only write russian/shirazi');}
 
 var numberOfpictures=prompt('how many pictures do you want?');
 
@@ -14,14 +17,23 @@ else if(favouritcat=='shirazi'){pictures = '<img src="pictures/shirazi.jfif"/>';
 else{alert('choose between russian or shirazi');}
 
 for(var i=0;i<numberOfpictures;i++){result+=pictures;}
-
-var amount =prompt("what is the amount that you can pay?");
-if (amount >= 100) {alert('you can pay')}
-else {alert('you caant buy')}
- 
-document.write(result);
+return result
+}
+document.write(getfavouritcat());
 
 
 
+var age =prompt("whats your age?");
+function getAge(userAge){ 
+console.log(age);
 
+if (userAge >= 18 && userAge <= 40){
+    alert('welcome to the website');
+   }else if(userAge < 18 || userAge > 40){
+    alert('sorry your not allowed');
+   }else {
+    alert('please input a number');
+    }
+ }
+getAge(age);
 
